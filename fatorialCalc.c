c86cd1df6a47b9be314115b79b56fbce71104733
bpoltosi <164111515+bpoltosi@users.noreply.github.com>
2026-08-11 23:01:06 -0300

    Validate input for negative numbers in factorial calculation
    Added validation for negative input and adjusted output messages.

--- fatorialCalc.c
+++ fatorialCalc.c
@@ -1,21 +1,27 @@
-/* calculo de valor do fatorial de 'n' */
+/* calculo de valor do fatorial de 'n', validando entrada de negativos */
 
 #include <stdio.h>
 
-int main (void) {
-	int valorN;
-	int valorTotalFatorial = 1;
+int main(void) {
+    int valorN;
+    int valorTotalFatorial = 1;
 
-	printf (" Digite o valor de 'n' : ");
-	scanf ("%d", &valorN );
+    printf("Digite o valor de 'n': ");
+    scanf("%d", &valorN);
 
-	if (valorN > 1) {
-		do {
-			valorTotalFatorial *= valorN;
-			valorN--;
-		}
-		while (valorN != 1);
-	}
-	printf (" Valor do fatorial de 'n' : %d", valorTotalFatorial);
-	return 0;
+    if (valorN < 0) {
+        printf("Nao existe fatorial de numero negativo.");
+    }
+    else {
+        if (valorN > 1) {
+            do {
+                valorTotalFatorial *= valorN;
+                valorN--;
+            } while (valorN != 1);
+        }
+
+        printf("Valor do fatorial: %d", valorTotalFatorial);
+    }
+
+    return 0;
 }
