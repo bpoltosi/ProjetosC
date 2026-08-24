e742b4daf8f2ebd8c3b20e6b4a333ff1a14698dc
bpoltosi <brunopoltosi@gmail.com>
2026-08-24 16:33:57 -0300

    Adicionar implementação para criação de matriz 3x4 com soma dos índices

---
+++ ListaMatrizes2/matriz5.c
@@ -0,0 +1,37 @@
+/* Criar uma matriz 3x4 onde cada elemento é a soma dos índices da sua
+posição dentro da matriz. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+
+    int matriz[3][4];
+
+    srand(time(NULL));      // gerar a matriz[3][4]
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++){
+            matriz[i][j] = rand() % 100;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {       // percorrer matriz
+        for (int j = 0; j < 4; j++)
+        {
+            matriz[i][j] == i+j;        // atribuir valores matriz[i][j] == i+j
+        }
+    }
+
+    printf("\nMatriz: \n");         // printar
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d, ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+return 0;
+}
