3bccb6615ffd3fb05df44d160ede36e994b1663b
bpoltosi <brunopoltosi@gmail.com>
2026-08-24 16:33:41 -0300

    Adicionar implementação para leitura de matriz 4x3 e exibição de elemento selecionado pelo usuário

---
+++ ListaMatrizes2/matriz2.c
@@ -0,0 +1,47 @@
+/* Ler uma matriz 4x3 com valores reais e mostrar qual é o elemento
+armazenado em uma linha L e coluna C fornecidas pelo usuário. */
+
+#include <stdio.h>
+
+int main(void){
+
+    int matriz [4][3];
+    int linha;
+    int coluna;
+
+    srand(time(NULL));
+    
+    for (int i = 0; i < 4; i++) {                    // gerar matriz[4][3]
+        for (int j = 0; j < 3; j++){
+            matriz[i][j] = rand() % 100;
+        }
+    }
+
+/*    for (int i = 0; i < 4; i++){                                            // receber matriz
+        for (int j = 0; j < 3; j++){
+            printf("Informe o valor correspondente a linha [%d] e coluna [%d]: ", i+1, j+1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+*/
+
+
+    printf("\nQual a linha do numero que voce deseja? ");                   // receber valor linha
+    scanf("%d", &linha);
+
+    while(linha > 4 || linha <= 0){                                         // validar linha
+        printf("valor inválido, tente novamente");
+        scanf("%d", &linha);
+    }
+
+    printf("Qual a coluna do numero que voce deseja? ");                    // receber valor coluna
+    scanf("%d", &coluna);
+    
+    while(coluna > 3 || coluna <= 0){                                     // validar coluna
+        printf("valor inválido, tente novamente");
+        scanf("%d", &coluna);
+    }
+    printf("\nValor da Matriz[%d][%d]: %d", linha, coluna, matriz[linha-1][coluna-1]);      // printar
+
+    return 0;
+}
