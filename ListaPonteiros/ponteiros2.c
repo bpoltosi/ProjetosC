5a095d60e9d2b66344909b0fad360d7780d79b12
Bruno Poltosi <164111515+bpoltosi@users.noreply.github.com>
2026-08-27 10:18:25 -0300

    Create ponteiros2.c

---
+++ ListaPonteiros/ponteiros2.c
@@ -0,0 +1,31 @@
+/*  Escreva um programa que leia dois números inteiros em variáveis "a" e "b" e 
+    troque os valores entre elas usando ponteiros.  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    
+int a;
+int b;
+    
+printf("Informe o valor de A: ");
+scanf("%d", &a);
+printf("Informe o valor de B: ");
+scanf("%d", &b);
+
+int *pa = a;
+int *pb = b;
+int aux;
+
+*pa = aux;
+*pa = *pb;
+*pb = aux;
+
+// Imprime A e B
+printf("Valor de A: %d", pa);
+printf("Valor de B: %d", pb);
+
+return 0;
+}
