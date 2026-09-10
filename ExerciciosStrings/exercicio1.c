6d6a7cbd306bd8293c0d33bec78cf5a70d91125e
Bruno Poltosi <164111515+bpoltosi@users.noreply.github.com>
2026-09-10 07:58:14 -0300

    Create exercicio1.c

---
+++ ExerciciosStrings/exercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char nome[20];
+    char cidade[]= "Santa Maria";
+    
+    printf("Digite seu nome: ");
+    scanf("%s\n", nome);
+    
+    printf("%s\n", cidade);
+    
+    return 0;
+}
