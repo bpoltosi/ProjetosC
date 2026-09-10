c9b7c2878027e29b2043de77c9a0037eb1235c0c
Bruno Poltosi <164111515+bpoltosi@users.noreply.github.com>
2026-09-10 08:01:06 -0300

    Create Exercicio3.c

---
+++ ExerciciosStrings/Exercicio3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char cidade[] = "Santa Maria";
+
+    printf("%s\n", cidade);
+    puts(cidade); // mesmo resultado, com quebra de linha automatica
+    
+    return 0;
+}
