95facb6782cc131f78217cc3440ae16fee55174b
bpoltosi <brunopoltosi@gmail.com>
2026-09-10 17:14:11 -0300

    Refatorar Exercicio2.c para melhorar a legibilidade e a estrutura do código

--- ListaStrings/Exercicio2.c
+++ ListaStrings/Exercicio2.c
@@ -1,34 +1,64 @@
-/* Desenvolver um programa que recebe uma palavra e o tipo de conversão que 
-o usuário deseja que ela sofra, que poderá ser letras maiúsculas ou minúsculas. 
+/* Desenvolver um programa que recebe uma palavra e
+o tipo de conversão que o usuário deseja que ela sofra, que poderá ser letras maiúsculas ou minúsculas.
 Observe que o campo (variável) tipo de conversão deverá ser validado. */
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-// Recebe entrada da palavra
-char palavra[100];
-printf("Informe uma palavra: ");
-fgets(palavra,100, stdin);                    
+int main()
+{
+	// Recebe entrada da palavra
+	char palavra[100];
+	printf("Informe uma palavra: ");
+	fgets(palavra, 100, stdin);
 
-// Tira o "espaco final"
-if (tam > 0 && palavra[tam - 1] == '\n') {
-	palavra[tam - 1] = '\0';
-	tam--;
-}
+	// Tira o "espaco final"
+	int tam = strlen(palavra);
+	if (tam > 0 && palavra[tam - 1] == '\n')
+	{
+		palavra[tam - 1] = '\0';
+		tam--;
+	}
 
-// Valida entrada
-if (tam <= 0) {
-	printf("Nenhuma palavra informada.\n");
+	// Valida entrada
+	if (tam <= 0)
+	{
+		printf("Nenhuma palavra informada.\n");
+		return 0;
+	}
+
+	// Recebe a escolha
+	int escolha = 0;
+	printf("Informe 1 para Maiusculas\n");
+	printf("Informe 2 para Minusculas\n");
+	scanf("%i", &escolha);
+
+	// Faz a alteração necessaria + validação
+	char *p = palavra;
+	switch (escolha)
+	{
+	case 1:
+		while (*p != '\0')
+		{
+			*p = toupper(*p);
+			p++;
+		}
+		break;
+
+	case 2:
+		while (*p != '\0')
+		{
+			*p = tolower(*p);
+			p++;
+		}
+
+	default:
+		printf("Escolha inválida!");
+		return 0;
+	}
+
+	printf("Resultado: %s", palavra);
+
 	return 0;
 }
-
-printf("Escolha 1 para Maiusculas\nEscolha 2 para Minusculas\n");
-
-// Coloca em minusculas
-while (*p != '\0' && *p != '\n') {
-  char atual = tolower(*p); 
-
-// Coloca em maiusculas
-while (*p != '\0' && *p != '\n') {
-  char atual = toupper(*p); 
